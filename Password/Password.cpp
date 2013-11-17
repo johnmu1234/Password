355275e128abc9b00b950f7a7c9dfc0f4bb223c0
johnmu1234 <j_mullarkey@hotmail.com>
2013-11-17 16:33:23 +0000

    Initial version of simple password generator
    Needs to have the "mykeys.txt" file (while debugging the file needs to
    be in the same directory as Password.cpp or in the same directory for
    release as the exe), containing user defined words that will be randomly
    combined into password phrases with a number added.

---
+++ Password/Password.cpp
@@ -0,0 +1,103 @@
+// PasswordGen.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+bool not_space( char c )
+{
+	return !isspace(c & 255);
+}
+bool space( char c )
+{
+	return 0 != isspace(c & 255);
+}
+
+bool notRequired( char c )
+{
+	string notRequiredChars = string( ".,![]{}()\';+\"\\0123456789`:" );
+	string::const_iterator it;
+	it = find( notRequiredChars.begin(), notRequiredChars.end(), c );
+	if( it != notRequiredChars.end() )
+		return true;
+	return false;
+}
+
+int myRand( int minNumber, int maxNumber )
+{
+	int random;
+	do
+	{
+		random = rand();
+
+	} while( (random < minNumber) || (random > maxNumber) );
+
+	return random;
+}
+
+// Have a generator mode - were the table is produced from a supplied text file - write the data to a serialization
+// Normal mode should just use serialised data
+int _tmain(int argc, _TCHAR* argv[])
+{
+	ifstream	infile;
+	map<string, int> words;
+
+	infile.open("mykeys.txt", ifstream::in );
+	if( !infile.is_open() )
+		return -1;
+
+	string line;
+	while( getline( infile, line ) )
+	{
+		string::iterator it = line.begin();
+		while( it != line.end() )
+		{
+			it = find_if(it, line.end(), not_space );
+
+			string::iterator it1 = find_if(it, line.end(), space );
+
+			string word = string(it, it1);
+
+			string::iterator it2 = word.begin();
+			string::iterator it3 = remove_if( it2, word.end(), notRequired );
+			string word2 = string(it2, it3);
+
+			string::size_type wordLen = word2.length();
+			if( (wordLen > 4) && (wordLen < 7) )
+			{
+			   word2[0] = toupper( word2[0] );
+				words[word2]++;
+			}
+
+			it = it1;
+		}
+	}
+	infile.close();
+
+	unsigned int randSeed = (unsigned int)time(NULL);
+	srand( randSeed );
+
+	int rand1 = myRand( 0, words.size() );
+	int rand2 = myRand( 0, words.size() );
+	int rand3 = myRand( 100, 1000 );
+
+	map<string, int>::iterator wit1 = words.begin();
+	for( int xx = 0; xx < rand1; xx++ )
+		wit1++;
+
+	map<string, int>::iterator wit2 = words.begin();
+	for( int xx = 0; xx < rand2; xx++ )
+		wit2++;
+
+	cout << wit1->first << wit2->first << rand3;
+	return 0;
+}
+
